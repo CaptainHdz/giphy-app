1fc5a5a6230a2e857d6c310164e0c508ff890cb5
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-02 13:38:26 -0600

    UI redesign

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Topics is going to be rendered into buttons to make a search request for each topic when clicked
-const topics = ["dogs", "cats", "cars", "fries"];
+const topics = ["dogs", "cats", "iron man", "fries"];
 
 // Function
 function displayButtons() {
@@ -9,7 +9,7 @@ function displayButtons() {
     let newBtn = $("<button>");
     newBtn.text(topics[i]);
     newBtn.addClass("topic")
-    newBtn.addClass("m-1 btn btn-primary shadow")
+    newBtn.addClass("m-1 btn shadow")
     $("#giphyBtnDiv").append(newBtn);
      }
 };
@@ -23,9 +23,8 @@ let searchInput = $("#topic-input").val().trim();
 topics.push(searchInput);
 console.log(topics);
 let newBtn = $("<button>");
-console.log(topics[topics.length-1])
-    newBtn.text(topics[topics.length-1]);
-    newBtn.addClass("topic m-1 btn btn-primary shadow");
+    newBtn.text(topics[topics.length-1]); //Selects the last string in the array
+    newBtn.addClass("topic m-1 btn shadow");
     $("#giphyBtnDiv").append(newBtn)
 // displayButtons();
 };
@@ -38,7 +37,7 @@ function displayTopicGif() {
 $("#giphyDiv").empty()
 console.log("Request in progress");
 const query = $(this).text()
-const mainURL = "http://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=hXBN5LO4EL4KjY60GVR4Pxz5Tyh5zcNN&limit=12"
+const mainURL = "https://api.giphy.com/v1/gifs/search?q="+ query + "&api_key=hXBN5LO4EL4KjY60GVR4Pxz5Tyh5zcNN&limit=12"
 $.ajax({
 url: mainURL,
 method: "GET"
